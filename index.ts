406e263064f005dc32fe314daf37682be1d2545b
Aryan Sawhney <240aryans@gmail.com>
2022-05-22 11:45:33 +0400

    fix skip code blocks when parsing #13

--- index.ts
+++ index.ts
@@ -1,141 +1,194 @@
-import '@logseq/libs';
-import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
+import "@logseq/libs";
+import { SettingSchemaDesc } from "@logseq/libs/dist/LSPlugin.user";
 // @ts-ignore
-import Sherlock from 'sherlockjs'
-import { getDateForPage } from 'logseq-dateutils'
+import Sherlock from "sherlockjs";
+import { getDateForPage } from "logseq-dateutils";
 
-let pageList: string[] = []
+let pageList: string[] = [];
 let blockArray: string[] = [];
 let dateFormat = "";
 
-const settings: SettingSchemaDesc[] = [{
-  key: "enableAutoParse",
-  description: "Automatically parse the block on enter",
-  type: "boolean",
-  default: false,
-  title: "Automatically parse the block on enter"
-},
-{
-  key: "stateKeybinding",
-  description: "Keybinding to toggle Automatic Parsing",
-  type: "string",
-  default: "mod+shift+l",
-  title: "Keybinding for Automatic Parsing"
-},
-{
-  key: "parseSingleBlockKeybinding",
-  description: "Keybinding to parse a single block",
-  type: "string",
-  default: "mod+p",
-  title: "Keybinding for Parsing a Single Block"
-},
-{
-  key: "parseSingleWordAsTag",
-  description: "Parse single words as tags",
-  type: "boolean",
-  default: false,
-  title: "Parse single words as tags"
-},
-{
-  key: "parseAsTags",
-  description: "Parse all links as tags",
-  type: "boolean",
-  default: false,
-  title: "Parse all links as tags"
-},
-{
-  key: "pagesToIgnore",
-  description: "Pages to ignore when generating links",
-  type: "string",
-  default: "a,b,c,card,now,later,todo,doing,done,wait,waiting,canceled,cancelled,started,in-progress",
-  title: "Pages to ignore when generating links"
-}]
-logseq.useSettingsSchema(settings)
+const settings: SettingSchemaDesc[] = [
+  {
+    key: "enableAutoParse",
+    description: "Automatically parse the block on enter",
+    type: "boolean",
+    default: false,
+    title: "Automatically parse the block on enter",
+  },
+  {
+    key: "stateKeybinding",
+    description: "Keybinding to toggle Automatic Parsing",
+    type: "string",
+    default: "mod+shift+l",
+    title: "Keybinding for Automatic Parsing",
+  },
+  {
+    key: "parseSingleBlockKeybinding",
+    description: "Keybinding to parse a single block",
+    type: "string",
+    default: "mod+p",
+    title: "Keybinding for Parsing a Single Block",
+  },
+  {
+    key: "parseSingleWordAsTag",
+    description: "Parse single words as tags",
+    type: "boolean",
+    default: false,
+    title: "Parse single words as tags",
+  },
+  {
+    key: "parseAsTags",
+    description: "Parse all links as tags",
+    type: "boolean",
+    default: false,
+    title: "Parse all links as tags",
+  },
+  {
+    key: "pagesToIgnore",
+    description: "Pages to ignore when generating links",
+    type: "string",
+    default:
+      "a,b,c,card,now,later,todo,doing,done,wait,waiting,canceled,cancelled,started,in-progress",
+    title: "Pages to ignore when generating links",
+  },
+];
+logseq.useSettingsSchema(settings);
 async function getPages() {
-  const pagesToIgnore = logseq.settings?.pagesToIgnore.split(',').map(x => x.toUpperCase().trim())
-  const query = `[:find (pull ?p [*]) :where [?p :block/uuid ?u][?p :block/original-name]]`
-  logseq.DB.datascriptQuery(query).then(
-    (results) => {
-      pageList = results
-        .filter(x => !pagesToIgnore.includes(x[0]['original-name'].toUpperCase()))
-        .map(x => x[0]['original-name'])
-    }
-  )
+  const pagesToIgnore = logseq.settings?.pagesToIgnore
+    .split(",")
+    .map((x) => x.toUpperCase().trim());
+  const query = `[:find (pull ?p [*]) :where [?p :block/uuid ?u][?p :block/original-name]]`;
+  logseq.DB.datascriptQuery(query).then((results) => {
+    pageList = results
+      .filter(
+        (x) => !pagesToIgnore.includes(x[0]["original-name"].toUpperCase())
+      )
+      .map((x) => x[0]["original-name"]);
+  });
 }
 const parseForRegex = (s: string) => {
   return s
-    .replaceAll("[", '\\[')
-    .replaceAll("]", '\\]')
-    .replaceAll(")", '\\)')
-    .replaceAll("(", '\\(')
-    .replaceAll("+", '\\+')
-    .replaceAll("-", '\\-')
-    .replaceAll("{", '\\{')
-    .replaceAll("}", '\\}')
-}
+    .replaceAll("[", "\\[")
+    .replaceAll("]", "\\]")
+    .replaceAll(")", "\\)")
+    .replaceAll("(", "\\(")
+    .replaceAll("+", "\\+")
+    .replaceAll("-", "\\-")
+    .replaceAll("{", "\\{")
+    .replaceAll("}", "\\}");
+};
+
 async function parseBlockForLink(d: string) {
-  let block = await logseq.Editor.getBlock(d)
+  let block = await logseq.Editor.getBlock(d);
   let content = block.content.replaceAll(/{.*}/g, (match) => {
-    return getDateForPage(Sherlock.parse(match.slice(1, -1)).startDate, dateFormat)
-  })
-  //rmeove first and last letter from the result 
-  let needsUpdate = false
+    return getDateForPage(
+      Sherlock.parse(match.slice(1, -1)).startDate,
+      dateFormat
+    );
+  });
+
+  //handle escaped content
+  let codeblockReversalTracker = [];
+  let inlineCodeReversalTracker = [];
+  let reversalIndexTracker = 0;
+
+  content = content.replaceAll(/```(.|\n)*```/gim, (match) => {
+    // reversalIndexTracker++;
+    console.log(codeblockReversalTracker);
+    codeblockReversalTracker.push(match);
+    return "wxhkjsdkdksjldfkjhsdfkncncn";
+  });
+
+  content = content.replaceAll(/(?=`)`(?!`)[^`]*(?=`)`(?!`)/g, (match) => {
+    inlineCodeReversalTracker.push(match);
+    return "zmkjfndkfhkfhjkdfkjdlhfkdljfkjd";
+  });
+
+  //rmeove first and last letter from the result
+  let needsUpdate = false;
 
   pageList.forEach((value) => {
-    const regex = new RegExp(`(\\w*(?<!\\[{2}[^[\\]]*)\\w*(?<!\\#)\\w*(?<!\\w+:\\/\\/\\S*))\\b(${parseForRegex(value)})(?![^[\\]]*\\]{2})\\b`, 'gi')
+    const regex = new RegExp(
+      `(\\w*(?<!\\[{2}[^[\\]]*)\\w*(?<!\\#)\\w*(?<!\\w+:\\/\\/\\S*))\\b(${parseForRegex(
+        value
+      )})(?![^[\\]]*\\]{2})\\b`,
+      "gi"
+    );
     if (value.length > 0) {
       if (content.toUpperCase().includes(value.toUpperCase())) {
         content = content.replaceAll(regex, (match) => {
-          const hasSpaces = /\s/g.test(match)
-          if (logseq.settings?.parseAsTags || (logseq.settings?.parseSingleWordAsTag && !hasSpaces)) {
-            return hasSpaces ? `#[[${value}]]` : `#${value}`
+          const hasSpaces = /\s/g.test(match);
+          if (
+            logseq.settings?.parseAsTags ||
+            (logseq.settings?.parseSingleWordAsTag && !hasSpaces)
+          ) {
+            return hasSpaces ? `#[[${value}]]` : `#${value}`;
           }
-          return `[[${value}]]`
-        })
-        needsUpdate = true
+          return `[[${value}]]`;
+        });
+        needsUpdate = true;
         // setTimeout(() => { inProcess = false }, 300)
       }
     }
-  })
+  });
   // logseq.Editor.updateBlock(block.uuid, content)
-  if(needsUpdate) {
-    logseq.Editor.updateBlock(block.uuid, `${content}`)
+
+  //re add the escaped content
+  codeblockReversalTracker.forEach((value, index) => {
+    content = content.replaceAll(`wxhkjsdkdksjldfkjhsdfkncncn`, value);
+  });
+  inlineCodeReversalTracker.forEach((value, index) => {
+    content = content.replaceAll(`zmkjfndkfhkfhjkdfkjdlhfkdljfkjd`, value);
+  });
+
+  if (needsUpdate) {
+    logseq.Editor.updateBlock(block.uuid, `${content}`);
   }
 }
 
-
 const main = async () => {
-  getPages()
-  dateFormat = (await logseq.App.getUserConfigs()).preferredDateFormat
+  getPages();
+  dateFormat = (await logseq.App.getUserConfigs()).preferredDateFormat;
   logseq.DB.onChanged((e) => {
     if (e.txMeta?.outlinerOp == "insertBlocks") {
       if (logseq.settings?.enableAutoParse) {
-        blockArray.forEach(parseBlockForLink)
+        blockArray.forEach(parseBlockForLink);
       }
     } else {
       //if blocks array doesn't already contain the block uuid, push to it
-      const block = e.blocks[0].uuid
+      const block = e.blocks[0].uuid;
       if (!blockArray.includes(block)) {
-        blockArray.push(block)
+        blockArray.push(block);
       }
     }
-  })
-  logseq.App.onCurrentGraphChanged(getPages)
+  });
+  logseq.App.onCurrentGraphChanged(getPages);
   logseq.Editor.registerBlockContextMenuItem("Parse Block for Links", (e) => {
     return parseBlockForLink(e.uuid);
-  })
-  logseq.App.registerCommandShortcut({ binding: logseq.settings?.stateKeybinding }, () => {
-    getPages()
-    blockArray = []
-    const enabledText = logseq.settings?.enableAutoParse ? 'disabled' : 'enabled'
-    logseq.App.showMsg(`Auto Parse Links ${enabledText}`)
-    logseq.updateSettings({ enableAutoParse: !logseq.settings?.enableAutoParse })
-  })
-  logseq.App.registerCommandShortcut({ binding: logseq.settings?.parseSingleBlockKeybinding }, (e) => {
-    getPages()
-    if (!logseq.settings?.enableAutoParse) {
-      parseBlockForLink(e.uuid)
+  });
+  logseq.App.registerCommandShortcut(
+    { binding: logseq.settings?.stateKeybinding },
+    () => {
+      getPages();
+      blockArray = [];
+      const enabledText = logseq.settings?.enableAutoParse
+        ? "disabled"
+        : "enabled";
+      logseq.App.showMsg(`Auto Parse Links ${enabledText}`);
+      logseq.updateSettings({
+        enableAutoParse: !logseq.settings?.enableAutoParse,
+      });
     }
-  })
-}
+  );
+  logseq.App.registerCommandShortcut(
+    { binding: logseq.settings?.parseSingleBlockKeybinding },
+    (e) => {
+      getPages();
+      if (!logseq.settings?.enableAutoParse) {
+        parseBlockForLink(e.uuid);
+      }
+    }
+  );
+};
 logseq.ready(main).catch(console.error);
